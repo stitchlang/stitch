876ecc3eacf56611d5e2831e0d62f25b23282228
Jonathan Marler <johnnymarler@gmail.com>
2021-01-08 10:18:42 -0700

    initial ideas

---
+++ README.md
@@ -0,0 +1,287 @@
+An exploration on a scripting language.  The basic syntax is:
+
+```
+program args...
+program args...
+```
+
+
+* how to interpret the program string
+    - if it contains slashes, it is a file path
+    - otherwise, it is a program in one of the PATH directories (NOTE: need builtin to resolve this, like $findprog)
+* if a command argument looks like one argument, it should be
+
+Special characters:
+
+Char | Description
+-----|-------------
+`$`  | starts an expression, escape with `$$`
+`(`  | start a command substitution, escape with `$(`
+`)`  | ends a command substitution, escape with `$)`
+
+> NOTE: instead of using `#` as a comment, the sequence `$#` can be used.  This reduces the number of special characters for which different modes would need to accomodate.  One thing to note is `$#` is processed earlier than other kinds of `$` escaping, that may be confusing.  Not sure if this is worth it or not.
+
+> NOTE: Say a line starts with `$foo`, if foo is a string, then it's expanded and it's interpreted as a program. If it's function (like $echo), then it's interpreted as that builtin function.
+
+> IDEA: maybe a script or a line should be able to change their special symbol from `$` to something else?  For example, you could have something like this `$->! !echo your balance is $1.25`.
+
+```
+$echo hello
+
+$# How to set/access a string variable
+$string name Fred
+$echo Hello $name
+
+$# How to set/use an array variable
+$array names args Fred Lisa Joey
+$echo Hello $expand.names
+
+$# Run a program and return it's output as a string
+$string arch ($oneline uname -m)
+```
+
+> NOTE: $echolines is just like $echo except it prints a newline after each argument
+```sh
+> $echolines arg1 arg2 arg3
+arg1
+arg2
+arg3
+
+> $echolines arg 1 arg 2 arg 3
+arg
+1
+arg
+2
+arg
+3
+
+> $echolines arg$space$1 arg$space$2 arg$space$3
+arg 1
+arg 2
+arg 3
+
+$# NOTE: maybe I could make space be something like $_ or $-, whatever I pick, note that no other symbol could start with it
+$#       $- might be better because some would probably expect $_foo to work
+> $echolines arg$-1 arg$-2 arg$-3
+> $echolines arg$_1 arg$_2 arg$_3
+> $echolines arg$,1 arg$,2 arg$,3
+
+$# NOTE: I can use command substituion to include spaces in a string like this
+> $echolines ($echo arg 1) ($echo arg 2) ($echo arg 3)
+arg 1
+arg 2
+arg 3
+
+
+$# NOTE: even with $echo, there may be cases where we want to include `$` characters without escaping them.  In this case, maybe I should include a HERDOC style?  (an example of this is calling awk)  I could support something a number of delimiters such as
+> $echolines $(arg 1) $[arg 2] $"arg 3" $'arg 4' $<arg 5> ${arg 6} $|arg 7|
+
+> awk $"{print $1 $2}"
+
+$# NOTE: I suppose these modes would disable `$` expansion?  Or maybe we want some modes to enable it?  Something to ponder on.
+$# Not sure if I should support this many styles?  I will probably also want to have some sort of multiline
+$# style of HERDOC, this one can take more characters.
+
+$echo $delimited EOF
+This is a raw multi-line
+string that does not end
+until it sees...
+EOF
+```
+
+> NOTE: I don't really like using "$ " to escape a space, because then the argument doesn't look like one argument.  This could also make the implementation a bit more complicated because the parser wouldn't be able to split arguments using whitespace before expansion.
+> TODO: should I support quoted arguments?  I'll leave them out for now since they aren't necessary for scripts to be funcationally complete.
+
+$# Variables
+
+```
+$#
+$# $string [SCOPE.]VARNAME VALUE
+$#
+$string msg Hello, my name is Fred
+$echo $msg
+$# prints "Hello, my name is Fred"
+
+$echo $g.msg
+$# still prints "Hello, my name is Fred", "g" is a special scope through which user variables can be accessed.  This should be used for variables that may conflict with predefined variables (i.e. $g.echo instead of $echo). (Note: `$g.g` is not the same as `$g`, it would be a user script variable named `g`).
+
+$string cpu_count ($oneline nproc)
+
+$echo You have $cpu_count cpus
+$echo Expand cpu count with a suffix is $cpu_count$cpus
+```
+
+
+
+Builtin variables
+
+Name   | C Equivalent
+-------|---------
+space  | The string " "
+lf     | The string "\n"
+cr     | The string "\r"
+crlf   | The string "\r\n"
+newline| Either "\n" or "\r\n" depending on platform
+tab    | The string "\t"
+g      | The script's global scope.
+env    | Environment variable scope.
+expand | Scope to expand arrays through.
+
+> NOTE: maybe support $XX$ to insert arbitrary hex characters?  So a space could be $20$?
+
+```
+$#
+$# $array [SCOPE.]VARNAME splitlines VALUE
+$# $array [SCOPE.]VARNAME args ARGS...
+$#
+$# TODO: splitwhitespace?
+$#
+$# Example:
+
+$array mounts splitlines (cat /proc/mounts)
+
+$# now the "mounts" variable is an array of all the mounts
+
+$# How to use the array in a command?
+$args-to-lines $expand.mounts
+
+$# Not using $expand.VAR will cause an error
+$args-to-lines $mounts
+$# Error: mounts is an array, you must expand it with $expand.mounts
+```
+
+Note that arrays cannot be used within another string.  They must be expanded on their own.  The reason for using the `expand` scope to expand arrays, is so that it's immediately apparent that an array is being expanded into 0 or more arguments.
+
+So we have 2 types, strings and arrays of strings. Is that all we need?
+We also have scopes, but so far string/arrays/scopes haven't had any intermixing in the grammar.
+
+### Environment variables:
+
+Environment variables are accessed in the `env` scope.
+
+```
+$env.VAR
+
+$string env.VAR VALUE
+```
+
+## Command Substitution
+
+```
+$# run command and return it's output as a string
+(PROG ARGS...)
+
+$string myfile_content (cat myfile)
+
+$# you can use the $oneline builtin to enforce that the command only prints one line and it will also trim the trailing newline
+$string lsfile ($oneline which ls)
+
+$# TODO: maybe have a $firstline/$lastline?
+
+$# Example
+make -j(nproc)
+```
+
+# Examples
+
+### Hello World
+
+```
+$echo Hello World
+```
+
+### Read input and print it
+
+```
+$string input (read)
+$echo You entered: $input
+```
+
+### Random
+
+```
+$string pwd ($oneline pwd)
+$string arch ($oneline uname -m)
+$string target $arch$-linux-musl
+$string prefix $env.HOME/$target
+$string jobs -j(nproc)
+
+wget example.come/$target$.tar.xz
+tar xf $target$.tar.xz
+
+$string env.PATH $pwd/$target/bin:$env.PATH
+```
+
+## Builtin Functions/Programs
+
+#### $oneline PROG ARGS...
+
+Runs the given program, enforces it only outputs one line and trims the trailing newline if it exists.
+
+# Grammar
+
+What I've got so far
+```
+Script ::= Command*
+
+Command ::= Arg*
+
+Arg ::= (Char | '$' DollarExpr)*
+
+DollarExpr ::= '(' Command ')' | [a-zA-Z_.]* '$'?
+```
+
+# Idea
+
+How to handle return code and stdout/stderr?  I could have a CommandResult type of object.
+```
+$#
+$# $captureouts PROG ARGS...
+$# $captureall     PROG ARGS...
+$#
+$# the $capture* builtins will run the program/args that follow, and instead of just returning stdout, it will return
+$# an object.  $capturestreams will capture stdout and stderr via the "out" and "err" fields.  $captureall will also capture
+$ the exit code in the field `code`, note that this overrides the default behavior of exiting on a non-zero exit code.
+
+$set some_program_result ($captureouts some-program)
+$echo the stdout of some_program_result is: $some_program_result.out
+$echo the stderr of some_program_result is: $some_program_result.err
+
+$set another_program_result ($captureouts another-program)
+$echo the stdout of another_program_result is: $another_program_result.out
+$echo the stderr of another_program_result is: $another_program_result.err
+$echo the exit code of another_program_result is: $another_program_result.code
+```
+
+# Idea: piping
+
+Piping is a pretty useful feature.  You could emulate piping by setting output to variables, however, piping directly from one process to another could remove extra copies of the output.
+
+A running process has 2 objects, the stdout and stderr file stream.  Once the process has finished, it returns 3 objects, a stdout string, a stderr string and an exit code.
+
+A script should be able to forward stdout and/or stderr from one process to the stdin of another.  Note that to forward stdout/stderr to a named file that can be read by another program, pipe files can be used.  This means the scripting language would need a way to redirect stdout and/or stderr to a named file as well.  Note that this could be done using a pipe operator that goes to a builtin command that forwards it to a file (means we don't need a redirect feature).
+
+Syntax
+```
+
+foo $out bar
+foo $err bar
+foo $outerr bar
+
+$# How to pipe stdout and stderr to different places?
+
+$# the following won't work because $err could belong to the previous command
+foo $out COMMAND... $err COMMAND...
+
+
+$# to redirect the output of "foo" to a file "myfile", use $in2file
+foo $out $in2file myfile
+
+$# to forward out/err to different places, could do multiple lines like this
+$set foo $stage foo args...
+$# the $stage COMMAND... builtin will create create a process that has not been started yet
+$# now $myprogram.out and $myprogram.err are open file handles
+$attach $foo.out bar
+$attach $foo.err baz
+$run $foo
+```
---
+++ prototype/main.py
@@ -0,0 +1,300 @@
+#!/usr/bin/env python3
+import sys
+import os
+import subprocess
+
+class Node:
+    pass
+class NodeRawString(Node):
+    def __init__(self, s):
+        self.s = s
+    def __repr__(self):
+        return "RawString({})".format(self.s)
+class NodeVariable(Node):
+    def __init__(self, id):
+        self.id = id
+    def __repr__(self):
+        return "Variable({})".format(self.id)
+class NodeCommandSub(Node):
+    def __init__(self, nodes):
+        self.nodes = nodes
+    def __repr__(self):
+        return "NodeCommandSub({})".format(", ".join([str(n) for n in self.nodes]))
+class NodeMultiple(Node):
+    def __init__(self, nodes):
+        self.nodes = nodes
+    def __repr__(self):
+        return "Multiple({})".format(", ".join([str(n) for n in self.nodes]))
+
+def combineNodes(existing_node, new_node):
+    if not existing_node:
+        return new_node
+    if type(existing_node) is NodeMultiple:
+        existing_node.nodes.append(new_node)
+        return existing_node
+    return NodeMultiple([existing_node, new_node])
+
+def isspaceOrd(c_ord):
+    return c_ord == ord(" ")
+
+def isIdOrd(c_ord):
+    if c_ord >= ord("a"):
+        return c_ord <= ord("z")
+    elif c_ord >= ord("A"):
+        return c_ord <= ord("Z") or c_ord == ord("_")
+    elif c_ord >= ord("0"):
+        return c_ord <= ord("9")
+    else:
+        return c_ord == ord(".")
+
+def skipWhitespace(src, i):
+    while i < len(src) and isspaceOrd(ord(src[i])):
+        i += 1
+    return i
+
+def parseDollarExpr(src, i):
+    if i == len(src):
+        # TODO: add test for this error
+        sys.exit("Error: got a '$' with nothing after it")
+    c_ord = ord(src[i])
+    if c_ord == ord("$"):
+        return NodeRawString("$"), i+1
+    if c_ord == ord("("):
+        return NodeRawString("("), i+1
+    if c_ord == ord(")"):
+        return NodeRawString(")"), i+1
+    if c_ord == ord("#"):
+        return NodeRawString("#"), i+1
+    if not isIdOrd(c_ord):
+        sys.exit("Error: expected [a-zA-Z0-9_.$] after '$' but got '{}'".format(chr(c_ord)))
+    id_start = i
+    while True:
+        i += 1
+        if i == len(src):
+            id_end = i
+            break
+        c_ord = ord(src[i])
+        if not isIdOrd(c_ord):
+            id_end = i
+            if c_ord == ord("$"):
+                i += 1
+            break
+    return NodeVariable(src[id_start:id_end]), i
+
+def parseNode(src, i):
+    assert(i < len(src))
+    assert(ord(src[i]) != "#")
+    assert(not isspaceOrd(ord(src[i])))
+    node = None
+    mark = i
+    while i < len(src):
+        c_ord = ord(src[i])
+        if c_ord == ord("$"):
+            if i > mark:
+                node = combineNodes(node, NodeRawString(src[mark:i]))
+            dollar_node, dollar_str_limit = parseDollarExpr(src, i+1)
+            node = combineNodes(node, dollar_node)
+            mark = dollar_str_limit
+            i = dollar_str_limit
+            continue
+        if c_ord == ord("("):
+            if i > mark:
+                node = combineNodes(node, NodeRawString(src[mark:i]))
+            cmd_start = i+1
+            cmd_subst_nodes, cmd_subst_limit = parseCommand(src, cmd_start)
+            if cmd_subst_limit == len(src) or ord(src[cmd_subst_limit]) != ord(")"):
+                # TODO: add test for this error
+                sys.exit("Error: '(' is missing the closing paren ')'")
+            node = combineNodes(node, NodeCommandSub(cmd_subst_nodes))
+            mark = cmd_subst_limit + 1
+            i = cmd_subst_limit + 1
+            continue
+        if isspaceOrd(c_ord) or c_ord == ord("#") or c_ord == ord(")"):
+            break
+        i += 1
+    if i > mark:
+        node = combineNodes(node, NodeRawString(src[mark:i]))
+    return node, i
+
+def parseCommand(src, i):
+    nodes = []
+    while True:
+        i = skipWhitespace(src, i)
+        if i == len(src):
+            break
+        next_ord = ord(src[i])
+        if next_ord == ord("#") or next_ord== ord(")"):
+            break
+        node, i = parseNode(src, i)
+        if not node:
+            break
+        nodes.append(node)
+    return nodes, i
+
+def parseTopLevelCommand(src):
+    nodes, _ = parseCommand(src, 0)
+    return nodes
+
+def stripNewline(s):
+    if len(s) > 0 and ord(s[-1]) == "\n":
+        if len(s) > 2 and s[-2] == "\r":
+            return s[:-2]
+        return s[:-1]
+    return s
+
+class Builtin:
+    def echo(args, script_vars):
+        output = " ".join(args)
+        print(output)
+        return output
+    def string(args, script_vars):
+        if len(args) == 0:
+            sys.exit("Error: the $string builtin requires at least one argument")
+        varname = args[0]
+        if len(args) > 2:
+            sys.exit("Error: the $string builtin can only accept 2 arguments but got {}".format(len(args)))
+        value = args[1]
+        print("DEBUG: setting string variable '{}' to '{}'".format(varname, value))
+        script_vars[varname] = ObjString(value)
+        # NOTE: not returning the value here? Why? because the $string builtin doesn't output it.  Also, if
+        #       a script wants the value, they can now acces it through the variable that is being set.
+        return ""
+    def oneline(args, script_vars):
+        if len(args) == 0:
+            sys.exit("Error: the $oneline builtin requires at least one argument")
+        s = runCommandExpanded(args, script_vars, False)
+        lines = s.splitlines()
+        if len(lines) == 0:
+            return ""
+        if len(lines) == 1:
+            return stripNewline(lines[0])
+        sys.exit("Error: program '{}' returned {} lines, but expected 1".format(str(args[0]), len(lines)))
+
+
+class Obj:
+    pass
+class ObjBuiltinFunc:
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return "${}".format(self.name)
+class ObjString:
+    def __init__(self, value):
+        self.value = value
+
+builtin_objects = {
+    "_": ObjString(" "),
+    "echo": ObjBuiltinFunc("echo"),
+    "string": ObjBuiltinFunc("string"),
+    "array": ObjBuiltinFunc("array"),
+    "oneline": ObjBuiltinFunc("oneline"),
+}
+
+def runCommandNodes(ast_nodes, script_vars):
+    #print("DEBUG: {}".format("  --  ".join([str(n) for n in nodes])))
+    return runCommandExpanded(expandNodes(ast_nodes, script_vars), script_vars, True)
+
+
+def which(name):
+    for path in os.environ["PATH"].split(os.pathsep):
+        filename = os.path.join(path, name)
+        if os.path.isfile(filename) and os.access(filename, os.X_OK):
+            return filename
+    return None
+
+
+def runCommandExpanded(exec_nodes, script_vars, log_cmd):
+    # I suppose this could happen if the whole command is just and expanded array that expands to nothing
+    if len(exec_nodes) == 0:
+        return ""
+    if log_cmd:
+        print("+ {}".format(" ".join([str(n) for n in exec_nodes])))
+    prog = exec_nodes[0]
+    if type(prog) == str:
+        # check args
+        for node in exec_nodes[1:]:
+            if type(node) != str:
+                sys.exit("The builtin function '{}' cannot be passed to an external program!".format(node))
+
+        if "/" in prog:
+            args = exec_nodes
+        else:
+            prog_filename = which(prog)
+            if not prog_filename:
+                sys.exit("Error: cannot find program '{}'".format(prog))
+            args = [prog_filename] + exec_nodes[1:]
+        # TODO: don't capture stderr
+        result = subprocess.run(args, capture_output=True)
+        if result.returncode != 0:
+            sys.exit("Error: program '{}' exited with code {}".format(prog, result.returncode))
+        # TODO: what to do with stderr?
+        if result.stdout:
+            return result.stdout.decode("utf8")
+        return ""
+    elif type(prog) == ObjBuiltinFunc:
+        return getattr(Builtin, prog.name)(exec_nodes[1:], script_vars)
+    else:
+        sys.exit("type is {}".format(type(prog)))
+
+def lookupVar(name, script_vars):
+    obj = builtin_objects.get(name)
+    if obj:
+        return obj
+    obj = script_vars.get(name)
+    if obj:
+        return obj
+    return None
+
+def concatPartAsString(part):
+    if type(part) == str:
+        return part
+    sys.exit("Error: can only concatenate strings but got a function '${}'".format(part))
+
+# returns an array of strings and builtin-function objects
+def expandNodes(nodes, script_vars):
+    exec_nodes = []
+    for node in nodes:
+        if type(node) is NodeRawString:
+            exec_nodes.append(node.s)
+        elif type(node) is NodeVariable:
+            obj = lookupVar(node.id, script_vars)
+            if not obj:
+                sys.exit("${} is undefined".format(node.id))
+            if type(obj) is ObjString:
+                exec_nodes.append(obj.value)
+            elif type(obj) is ObjBuiltinFunc:
+                exec_nodes.append(obj)
+            else:
+                sys.exit("not impl, expand object ${} of type {}".format(node.id, type(obj)))
+        elif type(node) is NodeCommandSub:
+            exec_nodes.append(runCommandNodes(node.nodes, script_vars))
+        elif type(node) is NodeMultiple:
+            sub_exec_nodes = expandNodes(node.nodes, script_vars)
+            exec_nodes.append("".join([concatPartAsString(n) for n in sub_exec_nodes]))
+        else:
+            raise Exception("codebug, unhandled node type {}".format(type(node)))
+    return exec_nodes
+
+def runFile(filename):
+    script_vars = {}
+    with open(filename, "r") as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            line = line.rstrip()
+            nodes = parseTopLevelCommand(line)
+            if len(nodes) == 0:
+                continue
+            _ = runCommandNodes(nodes, script_vars)
+
+def main():
+    cmd_args = sys.argv[1:]
+    if len(cmd_args) == 0:
+        sys.exit("Usage: prototype.py FILE")
+    if len(cmd_args) != 1:
+        sys.exit("Error: too many command-line arguments")
+    filename = cmd_args[0]
+    runFile(filename)
+
+main()
