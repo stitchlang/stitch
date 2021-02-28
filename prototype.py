ed42e3cfcd79f6a347f20de4865a6733764eb53f
Jonathan Marler <johnnymarler@gmail.com>
2021-02-28 14:12:50 -0700

    more Script Verification support

--- prototype.py
+++ prototype.py
@@ -204,7 +204,8 @@ class StitchObject:
 class Bool(StitchObject):
     def __init__(self, value):
         self.value = value
-    def userTypeDescriptor(self):
+    @staticmethod
+    def userTypeDescriptor():
         return "Bool"
 BOOL_FALSE = Bool(False)
 BOOL_TRUE = Bool(True)
@@ -214,17 +215,20 @@ class Builtin(StitchObject):
         self.name = name
     def __repr__(self):
         return "@{}".format(self.name)
-    def userTypeDescriptor(self):
+    @staticmethod
+    def userTypeDescriptor():
         return "Builtin"
 class String(StitchObject):
     def __init__(self, value):
         self.value = value
-    def userTypeDescriptor(self):
+    @staticmethod
+    def userTypeDescriptor():
         return "String"
 class Array(StitchObject):
     def __init__(self, elements):
         self.elements = elements
-    def userTypeDescriptor(self):
+    @staticmethod
+    def userTypeDescriptor():
         return "Array"
 
 class BinaryOperator(StitchObject):
@@ -244,7 +248,7 @@ class AndOperator(ChainableBinaryOperator):
     def apply(self, verification_mode: bool, stdout_handler, left: Bool, right):
         assert(verification_mode or left.value)
         right_result = operandToBool(stdout_handler, self, right)
-        if isinstance(right_result, Error):
+        if isinstance(right_result, Error) or type(right_result) == UnknownBool:
             return right_result
         return right_result
     def shortcircuit(self, result: Bool):
@@ -257,10 +261,10 @@ class OrOperator(ChainableBinaryOperator):
     def apply(self, verification_mode: bool, stdout_handler, left: Bool, right):
         assert(verification_mode or not left.value)
         right_result = operandToBool(stdout_handler, self, right)
-        if isinstance(right_result, Error):
+        if isinstance(right_result, Error) or type(right_result) == UnknownBool:
             return right_result
         return right_result
-    def shortcircuit(self, result):
+    def shortcircuit(self, result: Bool):
         return result.value
 class EqOperator(BinaryOperator):
     def __init__(self):
@@ -297,7 +301,8 @@ class CommandResult(StitchObject):
         self.stderr = stderr
         # indicates whethe multiline is allowed in toStringArg
         self.multiline = multiline
-    def userTypeDescriptor(self):
+    @staticmethod
+    def userTypeDescriptor():
         return "CommandResult"
     def toStringArg(self):
         # stderr not implemented
@@ -317,8 +322,32 @@ class CommandResult(StitchObject):
         return "CommandResult(exit={},stderr='{}',stdout='{}',multiline={})".format(
             self.exitcode, self.stderr, self.stdout, self.multiline)
 
+# an unknown value, this is used during verification
+class Unknown(StitchObject):
+    def __init__(self, stitch_type):
+        self.stitch_type = stitch_type
+
+class UnknownCommandResult(Unknown):
+    def __init__(self):
+        Unknown.__init__(self, CommandResult)
+    @staticmethod
+    def userTypeDescriptor():
+        return "CommandResult"
+UNKNOWN_COMMAND_RESULT = UnknownCommandResult()
+
+class UnknownBool(Unknown):
+    def __init__(self):
+        Unknown.__init__(self, Bool)
+    @staticmethod
+    def userTypeDescriptor():
+        return "Bool"
+UNKNOWN_BOOL = UnknownBool()
+
+
+
 class ScriptContext:
-    def __init__(self, scriptfile, callerworkdir, verification_mode):
+    def __init__(self, doverify, scriptfile, callerworkdir, verification_mode):
+        self.doverify = doverify
         self.script_specific_builtin_objects = {
             "scriptfile": String(scriptfile),
             # NOTE: callerworkdir will need to be forwarded to any sub-scripts
@@ -326,9 +355,8 @@ class ScriptContext:
             "callerworkdir": String(callerworkdir),
         }
         self.var_map = {}
+        self.verify_started = {}
         self.verification_mode = verification_mode
-        if self.verification_mode:
-            self.verification_call_files = set([])
 
 class CommandContext:
     def __init__(self, script: ScriptContext, parent: 'CommandContext', capture_stdout: bool, depth: int = 0,
@@ -347,7 +375,8 @@ class CommandContext:
         if self.capture_stdout:
             return output
         if len(output) > 0:
-            print(output, end="\n" if (output[-1] != "\n") else "")
+            if not self.script.verification_mode:
+                print(output, end="\n" if (output[-1] != "\n") else "")
         return None
 
 class BuiltinMethods:
@@ -393,7 +422,7 @@ class BuiltinMethods:
         # this should always be true when cmd_ctx.depth > 0
         assert(cmd_ctx.capture_stdout)
         result = runCommandNodes(cmd_ctx.nextBuiltin(), nodes)
-        if isinstance(result, Error):
+        if isinstance(result, Error) or type(result) == UnknownCommandResult or type(result) == UnknownBool:
             return result
         if type(result) == Bool:
             return SemanticError("@multiline does not accept Bool")
@@ -404,13 +433,15 @@ class BuiltinMethods:
         result = expandToBool(cmd_ctx, nodes, "@assert", allow_cmd_result=False)
         if isinstance(result, Error):
             return result
+        if type(result) == UnknownBool:
+            return UNKNOWN_COMMAND_RESULT
         assert(type(result) == Bool)
         if not result.value:
-            return AssertError()
+            return AssertError(" ".join([n.src for n in nodes]))
         return CommandResult(0, cmd_ctx.handleBuiltinOutput(""), None, False)
     def not_(cmd_ctx: CommandContext, nodes: List[Node]):
         result = expandToBool(cmd_ctx, nodes, "@assert", allow_cmd_result=True)
-        if isinstance(result, Error):
+        if isinstance(result, Error) or isinstance(result, UnknownBool):
             return result
         assert(type(result) == Bool)
         return BOOL_FALSE if result.value else BOOL_TRUE
@@ -424,7 +455,12 @@ class BuiltinMethods:
         program_file = args[0]
         if len(args) > 1:
             sys.exit("Error: @call with more than just a program not implemented")
-        return runFile(cmd_ctx.script, program_file, cmd_ctx.capture_stdout, top_level=False)
+        if cmd_ctx.script.verification_mode:
+            return UNKNOWN_COMMAND_RESULT
+        return runFile(cmd_ctx.script.doverify,
+                       program_file,
+                       cmd_ctx.script.script_specific_builtin_objects["callerworkdir"].value,
+                       cmd_ctx.capture_stdout)
 
 
 # TODO: not sure if the Error types will be exposed to stitch yet, or if they
@@ -438,8 +474,9 @@ class SemanticError(Error):
     def __repr__(self):
         return "SemanticError: {}".format(self.msg)
 class AssertError(Error):
-    def __init__(self):
-        Error.__init__(self, "an assertion failed")
+    def __init__(self, src):
+        Error.__init__(self, "@assert failed")
+        self.src = src
     def __repr__(self):
         return "AssertError"
 class NonZeroExitCodeError(Error):
@@ -457,9 +494,11 @@ class UnexpectedMultilineError(Error):
         self.cmd_result = cmd_result
 
 def opInvalidTypeError(op, operand):
-    return SemanticError("'{}' does not accept objects of type {}".format(op, objUserTypeDescriptor(operand)))
+    return SemanticError("'{}' does not accept objects of type {}".format(op, operand.userTypeDescriptor()))
 
-def operandToBool(stdout_handler, op, operand):
+def operandToBool(stdout_handler, op, operand) -> Union[Error,Bool,UnknownBool]:
+    if isinstance(operand, Error):
+        return operand
     if type(operand) == Bool:
         return operand
     if type(operand) == CommandResult:
@@ -467,15 +506,9 @@ def operandToBool(stdout_handler, op, operand):
         if operand.stdout != None:
             stdout_handler.handle(operand.stdout)
         return BOOL_TRUE if (operand.exitcode == 0) else BOOL_FALSE
+    if type(operand) == UnknownBool or type(operand) == UnknownCommandResult:
+        return UNKNOWN_BOOL
     return opInvalidTypeError(op, operand)
-
-
-def objUserTypeDescriptor(obj):
-    if type(obj) == str:
-        return "String"
-    if isinstance(obj, StitchObject):
-        return obj.userTypeDescriptor()
-    raise Exception("objUserTypeDescriptor does not support '{}' yet".format(obj))
 
 class CompareOp:
     def gt(left, right):
@@ -591,7 +624,7 @@ def expandNodes(cmd_ctx: CommandContext, nodes: List[Node]) -> Union[Error,Expan
 
     return ExpandNodes.ExternalProgram(args)
 
-def expandToBool(cmd_ctx: CommandContext, nodes: List[Node], builtin_name: str, allow_cmd_result: bool) -> Union[Error,Bool]:
+def expandToBool(cmd_ctx: CommandContext, nodes: List[Node], builtin_name: str, allow_cmd_result: bool) -> Union[Error,Bool,UnknownBool]:
     if len(nodes) == 0:
         return SemanticError("{} requires at least 1 argument".format(builtin_name))
 
@@ -605,7 +638,7 @@ def expandToBool(cmd_ctx: CommandContext, nodes: List[Node], builtin_name: str, 
 
     if type(result) == ExpandNodes.Builtin:
         result = getattr(BuiltinMethods, result.builtin.name)(cmd_ctx, nodes[1:])
-        if type(result) == Bool:
+        if type(result) == Bool or type(result) == UnknownBool:
             return result
         # TODO: there are probably more types to handle here
     else:
@@ -613,9 +646,12 @@ def expandToBool(cmd_ctx: CommandContext, nodes: List[Node], builtin_name: str, 
         result = runExternalProgram(cmd_ctx.script.verification_mode, cmd_ctx.capture_stdout, result.args)
     if isinstance(result, Error):
         return result
-    assert(type(result) == CommandResult)
+    is_unknown_cmd_result = type(result) == UnknownCommandResult
+    assert(type(result) == CommandResult or is_unknown_cmd_result)
     if not allow_cmd_result:
         return SemanticError("{} expects a Bool but got a CommandResult".format(builtin_name))
+    if is_unknown_cmd_result:
+        return UNKNOWN_BOOL
     if result.stdout:
         raise Exception("TODO")
     if result.stderr:
@@ -623,7 +659,7 @@ def expandToBool(cmd_ctx: CommandContext, nodes: List[Node], builtin_name: str, 
     return BOOL_TRUE if (result.exitcode == 0) else BOOL_FALSE
 
 
-def runCommandNodes(cmd_ctx: CommandContext, nodes: List[Node]) -> Union[Error,Bool,CommandResult]:
+def runCommandNodes(cmd_ctx: CommandContext, nodes: List[Node]) -> Union[Error,Bool,CommandResult,UnknownBool,UnknownCommandResult]:
     assert(len(nodes) > 0)
     if not cmd_ctx.script.verification_mode and cmd_ctx.builtin_prefix_count == 0:
         # todo: is ("+" * (depth+1)) too inneficient?
@@ -677,14 +713,16 @@ def objectToArgs(obj: StitchObject, args: List[str]) -> Error:
         args.append(arg)
     elif type(obj) == Array:
         args.extend(obj.elements)
-    elif isinstance(obj, Bool):
+    elif type(obj) == UnknownCommandResult:
+        args.append("UNKNOWN_COMMAND_RESULT")
+    elif type(obj) == Bool or type(obj) == UnknownBool:
         return SemanticError("cannot coerce Bool to String")
     elif isinstance(obj, BinaryOperator):
         return SemanticError("unexpected '{}'".format(obj))
     else:
         return SemanticError("TODO: implement objectToArgs for type {} ({})".format(type(obj), obj))
 
-def runExternalProgram(verification_mode: bool, capture_stdout: bool, args: List[str]) -> Union[Error,CommandResult]:
+def runExternalProgram(verification_mode: bool, capture_stdout: bool, args: List[str]) -> Union[Error,CommandResult,UnknownCommandResult]:
     if len(args) == 0:
         # NOTE: this can happen if the user executed an expanded empty array
         #       what should we do in this case?
@@ -693,7 +731,7 @@ def runExternalProgram(verification_mode: bool, capture_stdout: bool, args: List
         return SemanticError("got a command with no arguments, what should the language do here?")
 
     if verification_mode:
-        return CommandResult(0, "" if capture_stdout else None, None, multiline=False)
+        return UNKNOWN_COMMAND_RESULT
 
     prog = args[0]
 
@@ -754,11 +792,7 @@ def expandNode(cmd_ctx: CommandContext, node: Node) -> StitchObject:
         return obj
 
     if type(node) is NodeCommandSub:
-        result = runCommandNodes(cmd_ctx.nextDepth(), node.nodes)
-        assert(isinstance(result, Error) or
-               type(result) == Bool or
-               type(result) == CommandResult)
-        return result
+        return runCommandNodes(cmd_ctx.nextDepth(), node.nodes)
 
     if type(node) is NodeMultiple:
         args = []
@@ -805,7 +839,7 @@ def runBinaryExpression(cmd_ctx: CommandContext, nodes: List[Node], first_obj: S
             return SemanticError("'{}' and '@{}' cannot be chained".format(op, next_op.id))
 
 
-def runLine(script_ctx: ScriptContext, line, print_trace, capture_stdout):
+def runLine(script_ctx: ScriptContext, line, print_trace, capture_stdout) -> Union[Error,Bool,CommandResult,UnknownBool,UnknownCommandResult]:
     output = "" if capture_stdout else None
 
     nodes = parseTopLevelCommand(line)
@@ -823,10 +857,12 @@ def runLine(script_ctx: ScriptContext, line, print_trace, capture_stdout):
     #        print(msg)
 
     result = runCommandNodes(CommandContext(script_ctx, None, capture_stdout), nodes)
-    if isinstance(result, Error):
+    if (isinstance(result, Error) or
+        type(result) == Bool or
+        type(result) == UnknownBool or
+        type(result) == UnknownCommandResult):
         return result
-    if type(result) == Bool:
-        return result
+
     assert(type(result) == CommandResult)
     if capture_stdout:
         assert(type(result.stdout) == str)
@@ -836,12 +872,7 @@ def runLine(script_ctx: ScriptContext, line, print_trace, capture_stdout):
 
     return CommandResult(result.exitcode, output, result.stderr, result.multiline)
 
-def runFile(script_ctx: ScriptContext, filename: str, capture_stdout: bool, top_level: bool) -> CommandResult:
-    if not top_level and script_ctx.verification_mode:
-        script_ctx.verification_call_files.add(filename)
-        return CommandResult(0, output if capture_stdout else None, None, multiline=False)
-
-    output = ""
+def runFileHelper(script_ctx: ScriptContext, filename: str, capture_stdout: bool) -> CommandResult:
     with open(filename, "r") as file:
         while True:
             line = file.readline()
@@ -853,21 +884,58 @@ def runFile(script_ctx: ScriptContext, filename: str, capture_stdout: bool, top_
                 return result
             if type(result) == Bool:
                 return SemanticError("unhandled Bool")
-
-            assert(type(result) == CommandResult)
-            if capture_stdout:
-                assert(type(result.stdout) == str)
-                if len(result.stdout) > 0:
-                    output += result.stdout
-                    if result.stdout[-1] != "\n":
-                        output += "\n"
+            if type(result) == CommandResult:
+                if capture_stdout:
+                    assert(type(result.stdout) == str)
+                    if len(result.stdout) > 0:
+                        output += result.stdout
+                        if result.stdout[-1] != "\n":
+                            output += "\n"
+                else:
+                    assert(result.stdout == None)
+                if result.exitcode != 0:
+                    return CommandResult(result.exitcode, output if capture_stdout else None, None, multiline=False)
             else:
-                assert(result.stdout == None)
-
-            if result.exitcode != 0:
-                return CommandResult(result.exitcode, output if capture_stdout else None, None, multiline=False)
+                assert(script_ctx.verification_mode and (
+                    type(result) == UnknownBool or
+                    type(result) == UnknownCommandResult
+                ))
 
     return CommandResult(0, output if capture_stdout else None, None, multiline=False)
+
+def normalizeFilename(filename):
+    # TODO: implement this
+    return filename
+
+def runFile(doverify: bool, full_filename: str, callerworkdir: str, capture_stdout: bool) -> CommandResult:
+
+    output = ""
+    #if not top_level and script_ctx.verification_mode:
+    #    return CommandResult(0, output if capture_stdout else None, None, multiline=False)
+
+    #full_filename = if os.path.isabsolute(filename) else os.path.os.path.abspath(filename)
+    script_ctx = ScriptContext(doverify, full_filename, callerworkdir, verification_mode=False)
+    if script_ctx.doverify:
+        normalized_filename = normalizeFilename(full_filename)
+        if not normalized_filename in script_ctx.verify_started:
+            script_ctx.verify_started[normalized_filename] = True
+            script_ctx.verification_mode = True
+            print("stitch: DEBUG: verifying '{}'".format(full_filename))
+            result = runFileHelper(script_ctx, full_filename, capture_stdout)
+            script_ctx.verification_mode = False
+            if isinstance(result, Error):
+                # This can happen if the arguments of an assert are known at "verification time"
+                # i.e. @assert @true
+                if type(result) == AssertError:
+                    print("{}: AssertError: {}".format(full_filename, result.src))
+                else:
+                    assert(type(result) == SemanticError)
+                    print("{}: SemanticError: {}".format(full_filename, result.msg))
+                return result
+            print("stitch: DEBUG: verification done on '{}'".format(full_filename))
+
+    return runFileHelper(script_ctx, full_filename, capture_stdout)
+
 
 def main():
     cmd_args = sys.argv[1:]
@@ -889,29 +957,11 @@ def main():
     callerworkdir = os.getcwd()
     os.chdir(sandbox_path)
 
-    #
-    # TODO: implement this
-    #
-    verify_done = False
-    verify_context = ScriptContext(filename, callerworkdir, verification_mode=True)
-    print("stitch: DEBUG: verifying '{}'".format(full_filename))
-    result = runFile(verify_context, full_filename, capture_stdout=False, top_level=True)
+    doverify = True
+    result = runFile(doverify, full_filename, callerworkdir, capture_stdout=False)
     if isinstance(result, Error):
-        assert(type(result) == SemanticError)
-        print("{}: SemanticError: {}".format(filename, result.msg))
-        sys.exit(1)
-        verify_done = True
-    print("stitch: DEBUG: verification done on '{}'".format(full_filename))
-    print("stitch: DEBUG: verification call files: {}".format(len(verify_context.verification_call_files)))
-    for call_file in verify_context.verification_call_files:
-        print("stitch: DEBUG: detected @call '{}'".format(call_file))
-
-    run_context = ScriptContext(full_filename, callerworkdir, verification_mode=False)
-    result = runFile(run_context, full_filename, capture_stdout=False, top_level=True)
-    if isinstance(result, Error):
-        assert((type(result) != SemanticError) or (not verify_done))
-        kind = "SemanticError" if (type(result) == SemanticError) else "Error"
-        print("{}: {}: {}".format(filename, kind, result.msg))
+        prefix = "Semantic" if (type(result) == SemanticError) else ""
+        print("{}: {}Error: {}".format(filename, prefix, result.msg))
         sys.exit(1)
 
     assert(type(result) == CommandResult)
